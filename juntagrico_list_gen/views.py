cf75c06ca09b56933c8d6054969fa0ea299064f6
flurin-conradin <f.conradin@gmail.com>
2020-02-06 21:27:53 +0100

    introduce threads

--- juntagrico_list_gen/views.py
+++ juntagrico_list_gen/views.py
@@ -1,19 +1,30 @@
 import json
-import time
+import logging
+import threading
 
 from django.contrib.auth.decorators import permission_required
 from django.core.files.storage import default_storage
 from django.core.management import call_command
 from django.http import Http404, HttpResponse
 
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+
+def list_gen_thread():
+    logging.info("Starting list generation: Pack list")
+    call_command("cs_generate_pack_list")
+    logging.info("Starting list generation: Depot list")
+    call_command("cs_generate_depot_list")
+    logging.info("List generation done")
+
 
 @permission_required("juntagrico.is_operations_group")
 def generate_lists(request):
-    print("generating lists")
-    call_command("cs_generate_depot_list")
-    call_command("cs_generate_pack_list")
-    data = json.dumps({"list_generation": "success"})
-    return HttpResponse(data, content_type="application/json")
+    thread = threading.Thread(target=list_gen_thread)
+    thread.start()
+    data = json.dumps({"list_generation": "started"})
+    return HttpResponse(data, status=202, content_type="application/json")
 
 
 @permission_required("juntagrico.is_operations_group")
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(requirements_file):
 
 setup(
     name='juntagrico-list-gen',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',
