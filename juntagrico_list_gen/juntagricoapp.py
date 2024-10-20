245621d9cbff953999835d6a3cc6bb865ec85383
flurin-conradin <f.conradin@gmail.com>
2024-10-20 08:49:01 +0200

    use new way of overwriting admin menu

--- juntagrico_list_gen/juntagricoapp.py
+++ juntagrico_list_gen/juntagricoapp.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from juntagrico.util import addons
 
 
 class ListGenConfig(AppConfig):
-    name = 'juntagrico_list_gen'
-
-
-addons.config.register_admin_menu('lg/menu_generate_lists.html')
+    name = "juntagrico_list_gen"
